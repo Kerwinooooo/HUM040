10671f8726dbfbb921cec3933d9be76d2c947db4
Kerwinooooo <60419837+Kerwinooooo@users.noreply.github.com>
2021-10-29 01:48:16 +0800

    update

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -7,15 +7,19 @@ from wordseg import WordSegment
 import pkuseg
 import difflib
 
-
 jiebaWordSet = set()
-
+fileName = ''
+newWordFileName = ''
 
 def getText() -> str:
+    global fileName
     inputText = input("text or file path:")
     try:
         file = open(file=inputText, mode='r', encoding='utf-8')
-        return file.read()
+        text = file.read()
+        file.close()
+        fileName = inputText.rsplit('.')[0]
+        return text
     except Exception as e:
         print(e)
         return inputText
@@ -36,15 +40,17 @@ def loadJiebaWordSet():
 
 
 def getNewWordList(orgText: str) -> list:
+    global newWordFileName
     newWordList = []
     ws = WordSegment(orgText, max_word_len=10, min_freq=0.00005, min_entropy=0.4, min_aggregation=1.2)
 
     for word in ws.words:
-        if word not in jiebaWordSet and ' ' not in word:
+        if word not in jiebaWordSet and ' ' not in word:  # remove the 'new word' that contains space
             newWordList.append(word)
             # print(word)
 
-    file = open(file='newWord_' + str(int(time.time())) + '.txt', mode='w', encoding='utf-8')
+    newWordFileName = fileName + '_newWord_' + str(int(time.time())) + '.txt'
+    file = open(file=newWordFileName, mode='w', encoding='utf-8')
     file.write('\n'.join(newWordList))
     file.close()
 
@@ -54,13 +60,13 @@ def getNewWordList(orgText: str) -> list:
 def segment():
     segWithoutNewWord = pkuseg.pkuseg()
     resultWithoutNewWord = segWithoutNewWord.cut(orgText)
-    file = open(file='result_without_new_word_' + str(int(time.time())) + '.txt', mode='w', encoding='utf-8')
+    file = open(file=fileName + '_result_without_new_word_' + str(int(time.time())) + '.txt', mode='w', encoding='utf-8')
     file.write(' '.join(resultWithoutNewWord))
     file.close()
 
-    segWithNewWord = pkuseg.pkuseg(user_dict='newWord.txt')
+    segWithNewWord = pkuseg.pkuseg(user_dict=newWordFileName)
     resultWithNewWord = segWithNewWord.cut(orgText)
-    file = open(file='result_with_new_word_' + str(int(time.time())) + '.txt', mode='w', encoding='utf-8')
+    file = open(file=fileName + '_result_with_new_word_' + str(int(time.time())) + '.txt', mode='w', encoding='utf-8')
     file.write(' '.join(resultWithNewWord))
     file.close()
 
@@ -83,3 +89,5 @@ if __name__ == '__main__':
     timeSegmentStart = time.time()
     segment()
     print(f'segment: {time.time() - timeSegmentStart}')
+
+    print('finished')
